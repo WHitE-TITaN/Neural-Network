ac736b9e5c2ff3d60a05946b8948913c50f75043
WHitE-TITaN <dk488621@gmail.com>
2025-03-19 22:30:52 +0530

    auto size array

--- mainDriver.cpp
+++ mainDriver.cpp
@@ -6,9 +6,12 @@ using namespace std;
 int main(){
     vector<int> input;
     int number;
-    while(cin>>number){
+    while(cin >> number){
         input.push_back(number);
     }
+
+    cin.clear(); // if any break occures clean it
+    cin.ignore();
 
     for(auto numebr : input){
         cout<<numebr<<", ";
