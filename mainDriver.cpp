e4f32374d4623cb90fcf5598b34acf19e7757486
WHitE-TITaN <dk488621@gmail.com>
2025-03-20 23:29:43 +0530

    check random function

--- mainDriver.cpp
+++ mainDriver.cpp
@@ -4,16 +4,19 @@
 using namespace std;
 
 int main(){
-    vector<int> input;
-    int number;
-    while(cin >> number){
-        input.push_back(number);
-    }
+    // vector<int> input;
+    // int number;
+    // while(cin >> number){
+    //     input.push_back(number);
+    // }
 
-    cin.clear(); // if any break occures clean it
-    cin.ignore();
+    // cin.clear(); // if any break occures clean it
+    // cin.ignore();
 
-    for(auto numebr : input){
-        cout<<numebr<<", ";
-    }
+    // for(auto numebr : input){
+    //     cout<<numebr<<", ";
+    // }
+
+    int GraterNumber = rand();
+    cout<<GraterNumber;
 }
