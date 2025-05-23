e13cd5c71a9a97b6ecadcc94603e202e224c1e6a
WHitE-TITaN <dk488621@gmail.com>
2025-05-23 14:28:44 +0530

    added cmake and maked include file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "../headers/transformer.h"
+#include "../headers/wordEmbedding.h"
 
 int main() {
 	transformer sol;
@@ -7,4 +8,16 @@ int main() {
 	for (auto character : hello) {
 		cout << character;
 	}
+
+	std::string input;
+	std::getline(cin, input);
+
+	wordEmbedding tokenizer1;
+	std::vector<std::string> output = tokenizer1.tokenize(input);
+	
+	for(auto& word : output){
+		std::cout<<word;	
+	}
+
+	return 0;
 }
