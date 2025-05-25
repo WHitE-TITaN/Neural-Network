7f309b42259a7d94cf219b9f0a95e882119c2e25
WHitE-TITaN <dk488621@gmail.com>
2025-05-25 09:39:58 +0530

    added offline model to power

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,8 @@
-#include "../headers/transformer.h"
-#include "../headers/wordEmbedding.h"
+#include <iostream>
+#include <vector>
+#include "llama.h"
 
 int main() {
-	transformer sol;
-	string hello = sol.sayHello();
-
-	for (auto character : hello) {
-		cout << character;
-	}
-
-	std::string input;
-	std::getline(cin, input);
-
-	wordEmbedding tokenizer1;
-	std::vector<std::string> output = tokenizer1.tokenize(input);
-	
-	for(auto& word : output){
-		std::cout<<word;	
-	}
-
+	std::cout<<"Hello";
 	return 0;
 }
