6288ebe037ea5e3891458fd108171b3b369f9d2c
WHitE-TITaN <dk488621@gmail.com>
2025-06-09 10:44:12 +0530

    fixed major redefination issue

--- headers/model.h
+++ headers/model.h
@@ -37,19 +37,19 @@ class assistant{
             llama_backend_init();
             
             //loading model in
-            llama_model_params model_params = llama_model_default_params();
-            llama_model* model = llama_model_load_from_file("C:/Users/dk488/Documents/GitHub/Neural-Network/headers/tinyllama.gguf", model_params);
+            model_params = llama_model_default_params();
+            model = llama_model_load_from_file("C:/Users/dk488/Documents/GitHub/Neural-Network/headers/tinyllama.gguf", model_params);
             if (!model) {
                 std::cerr << "❌ Failed to load model\n";
                 return;
             }
 
             //creating context..
-            llama_context_params ctx_params = llama_context_default_params();
+            ctx_params = llama_context_default_params();
             ctx_params.n_ctx = 2048;
-            llama_context* ctx = llama_init_from_model(model, ctx_params);
+            ctx = llama_init_from_model(model, ctx_params);
 
-            const llama_vocab* vocab = llama_model_get_vocab(model);
+            vocab = llama_model_get_vocab(model);
         }
 
 
